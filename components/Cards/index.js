d07c422092d80103eae1b1e10ddfc6c19fc90c4c
Vincent Sanders <vincesanders@hotmail.com>
2019-12-06 10:35:28 -0600

    Completed the cards component.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,35 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(res => {
+    console.log(res);
+    const topics = Object.values(res.data.articles);
+    console.log(topics);
+    for (art of topics) {
+        console.log(art);
+        art.forEach(art => {
+        const card = document.createElement('div');
+        const headline = document.createElement('div');
+        const author = document.createElement('div');
+        const imgContainer = document.createElement('div');
+        const img = document.createElement('img');
+        const authorName = document.createElement('span');
+
+        card.classList.add('card');
+        headline.classList.add('headline');
+        author.classList.add('author');
+        imgContainer.classList.add('img-container');
+
+        headline.textContent = art.headline;
+        img.src = art.authorPhoto;
+        authorName.textContent = art.authorName;
+        
+        imgContainer.append(img);
+        author.append(imgContainer, authorName);
+        card.append(headline, author);
+        document.querySelector('.cards-container').append(card);
+        });       
+    }
+}).catch(err => {
+    console.log(err);
+});
