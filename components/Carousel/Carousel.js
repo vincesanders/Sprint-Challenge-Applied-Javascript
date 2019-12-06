368db2d7f2547c462d7dc648728a93a605811a17
Vincent Sanders <vincesanders@hotmail.com>
2019-12-06 10:57:34 -0600

    Started working on the carousel component. Made adjestment to TopBar.less to correct bug of content appearing on top of navbar.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,33 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carouselCreator() {
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const img1 = document.createElement('img');
+  const img2 = document.createElement('img');
+  const img3 = document.createElement('img');
+  const img4 = document.createElement('img');
+  const rightButton = document.createElement('div');
+
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  img1.src = './assets/carousel/mountains.jpeg';
+  img2.src = './assets/carousel/computer.jpeg';
+  img3.src = './assets/carousel/trees.jpeg';
+  img4.src = './assets/carousel/turntable.jpeg';
+
+  leftButton.textContent = " < ";
+  rightButton.textContent = " > ";
+
+  carousel.append(leftButton, img1, img2, img3, img4, rightButton);
+
+  img1.style.display = 'inline-block';
+
+  document.querySelector('.carousel-container').appendChild(carousel);
+}
+
+carouselCreator();
